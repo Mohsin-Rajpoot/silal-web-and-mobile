330d5878b8c6e486ed07ff630f955fafa391551d
saadiButt-1995 <saadmb1995@gmail.com>
2022-03-08 12:44:38 +0500

    adding assets

---
+++ packages/common/assets/colors/index.js
@@ -0,0 +1,17 @@
+const sidebar = '#4C6870'
+const active_bar = '#5AB3A8'
+const nested_list = '#5AB3A8'
+const light_grey = '#CCD4D6'
+const purple = '#8777D9'
+const black = '#002733'
+const light_blue = '#DEEBFF'
+const dark_blue = '#0052CC'
+const light_green = '#34C759'
+const red = '#FF5630'
+const orange = '#FF8F73'
+
+
+export {
+    sidebar, active_bar, nested_list, light_grey, purple,
+    black, light_blue, dark_blue, light_green, red, orange 
+}
---
+++ packages/common/assets/icons/index.js
@@ -0,0 +1,14 @@
+import Coupons from './coupons.svg'
+import Database from './database.svg'
+import Inquiries from './inquiries.svg'
+import Dashboard from './dashboard.svg'
+import Settings from './settings.svg'
+
+
+export {
+    Coupons,
+    Database,
+    Inquiries,
+    Dashboard,
+    Settings
+}
