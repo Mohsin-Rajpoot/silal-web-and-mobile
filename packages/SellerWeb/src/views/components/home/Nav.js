9a8866c974aa31d78dec1a6e510d98b83e6ccee3
saadiButt-1995 <saadmb1995@gmail.com>
2022-03-16 22:42:16 +0500

    test1 16-3

--- packages/SellerWeb/src/views/components/home/Nav.js
+++ packages/SellerWeb/src/views/components/home/Nav.js
@@ -50,45 +50,4 @@ const Nav = ({
     )
 }
 
-export const NavStyled = styled.nav`
-    margin-top: 19px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding-bottom: 10px;
-    .menu {
-        border: none;
-        background-color: transparent;
-        margin-right: 20px;
-        line-height: 1;
-        margin-bottom: 15px;
-        svg {
-            width: 19px;
-        }
-    }
-    .nav-link-btn {
-        display: block;
-        margin-right: 20px;
-        padding: 5px 10px;
-        height: 32px;
-        border-radius: 5px;
-        text-decoration: none;
-        color: #4C6870;
-        font-weight: 600;
-        font-size: 16px;
-        margin-bottom: 15px;
-        white-space: nowrap;
-        &.active,
-        &:hover {
-            background: var(--theme-clr);
-            color: white;
-        }
-    }
-    .link {
-        svg {
-            width: 42px;
-        }
-    }
-`;
-
 export default Nav
