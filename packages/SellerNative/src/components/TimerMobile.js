cf20ff656bbdb8183a8ba74eca979c136720e747
Mshoaib41 <shoaibbabar9612@gmail.com>
2022-06-13 12:35:42 +0500

    estimated timer

--- packages/SellerNative/src/components/TimerMobile.js
+++ packages/SellerNative/src/components/TimerMobile.js
@@ -1,4 +1,4 @@
-import {Text, View, TouchableOpacity, TextInput} from 'react-native';
+import {Text, View, ScrollView} from 'react-native';
 import React, {useState, useRef} from 'react';
 import {scale, ScaledSheet, verticalScale} from 'react-native-size-matters';
 import Entypo from 'react-native-vector-icons/Entypo';
@@ -8,90 +8,146 @@ import colors from '@SilalApp/common/assets/colors';
 import Toast from 'react-native-easy-toast';
 import CustomText from '@SilalApp/common/components/CustomText';
 import fonts from '@SilalApp/common/assets/fonts';
+import TextInput from '../../../common/components/native/TextInput/index';
+import {CustomButton} from '@SilalApp/common/components/native';
+import {useTranslation} from 'react-i18next';
+
 
 const TimerMobile = ({label}) => {
+  const {t} = useTranslation();
   const [modal_timer_visible, set_modal_timer_visible] = useState(false);
   const [values, set_values] = useState(0);
   const [manual_time, set_manual_time] = useState('');
   const toastRef = useRef();
-    const emptyArray = [0,1,2,3,4,5,6]
-    const timeArray = ["-15 min","-1 min","-5 min","0 min","5 min","10 min","15 min"]
+  const emptyArray = [0,1];
+  const timeArray = ['50 min', '40 min',  '60 min'];
   return (
-    <View style={styles.centeredView}>
-      <TouchableOpacity
-      style={{position:'absolute',top:5,right:5}}
-        onPress={() => set_modal_timer_visible(!modal_timer_visible)}>
-        <Entypo name="cross" color={colors.black} size={verticalScale(20)} />
-      </TouchableOpacity>
+    <ScrollView style={styles.centeredView}>
+     
       <CustomText
         fontSize={18}
         fontFamily={fonts.bold}
         marginBottom={verticalScale(5)}
-        label={label ? label : ' Order will be ready in:'}
+        label={label ? label : t("Order_will_be_ready")}
       />
       <CustomText
         fontSize={18}
         fontFamily={fonts.bold}
         color={colors.primary}
-        label={values + 'minutes'}
+        label={values + 40  + t("minutes")}
       />
 
       <View style={{marginTop: verticalScale(20), width: '100%'}}>
-            <Slider
-              value={values}
-              onValueChange={set_values}
-              maximumValue={15}
-              minimumValue={-15}
-              step={5}
-              // allowTouchTrack
-              minimumTrackTintColor="#CCD4D6"
-              maximumTrackTintColor="#CCD4D6"
-              // trackStyle={{ height: 5, backgroundColor: '#CCD4D6' }}
-              thumbStyle={{
-                height: 30,
-                width: 30,
-                backgroundColor: 'transparent',
-              }}
-              thumbProps={{
-                children: (
-                  <View
-                    style={{
-                      backgroundColor: 'white',
-                      height: 30,
-                      width: 30,
-                      borderRadius: verticalScale(20),
-                      borderWidth: 2,
-                      borderColor: colors.primary,
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                    }}>
-                    <View
-                      style={{
-                        backgroundColor: colors.primary,
-                        height: 10,
-                        width: 10,
-                        borderRadius: 20,
-                      }} />
-                  </View>
-                ),
-              }}
-            />
+        <Slider
+          value={values}
+          onValueChange={set_values}
+          maximumValue={10}
+          minimumValue={-10}
+          step={10}
+          // allowTouchTrack
+          minimumTrackTintColor="#CCD4D6"
+          maximumTrackTintColor="#CCD4D6"
+          // trackStyle={{ height: 5, backgroundColor: '#CCD4D6' }}
+          thumbStyle={{
+            height: verticalScale(40),
+            width: verticalScale(40),
+            backgroundColor: 'transparent',
+          }}
+          thumbProps={{
+            children: (
+              <View
+                style={{
+                  backgroundColor: 'white',
+                  height: verticalScale(40),
+                  width: verticalScale(40),
+                  borderRadius: verticalScale(100),
+                  borderWidth: 2,
+                  borderColor: colors.primary,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <View
+                  style={{
+                    backgroundColor: colors.primary,
+                    height: verticalScale(8),
+                    width: verticalScale(8),
+                    borderRadius: verticalScale(100),
+                  }}
+                />
+              </View>
+            ),
+          }}
+        />
 
-            <View style={{flexDirection:'row',alignItems:'center',justifyContent:'space-between',paddingHorizontal:scale(12)}}>
-            {emptyArray.map((item,index)=>{
-                return(
-                    <View key={index} style={styles.heightView}/>
-                )
-            }) }
-            </View>
-            <View style={{flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
-            {timeArray.map((item,index)=>{
-                return(
-                    <CustomText fontSize={verticalScale(7)} textStyle={{width: 35,}} key={index} label={item} />
-                )
-            }) }
-            </View>
-            {/* <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+      </View>
+      <View style={{width: '100%',}}>
+      <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            marginTop:verticalScale(10),
+            paddingHorizontal:scale(20)
+          }}>
+          {emptyArray.map((item, index) => {
+            return <View key={index} style={styles.heightView} />;
+          })}
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            // paddingHorizontal:scale(20)
+          }}>
+          {timeArray.map((item, index) => {
+            return (
+              <CustomText
+                fontSize={verticalScale(10)}
+                textStyle={{width: scale(42), textAlign: 'center'}}
+                key={index}
+                label={item}
+              />
+            );
+          })}
+        </View>
+      </View>
+     <View style={{alignSelf:'flex-start'}}>
+     <CustomText
+        label={t("enter_manually")}
+        fontSize={15}
+        marginBottom={verticalScale(8)}
+        fontFamily={fonts.LatoMedium}
+        marginTop={verticalScale(15)}
+      />
+     </View>
+      <TextInput
+        placeholderText="+30 min"
+        value={manual_time}
+        keyboardType="decimal-pad"
+        onChangeText={text => set_manual_time(text)}
+        inputTextStyle={{width: '100%'}}
+        inputStyle={{
+          width: '100%',
+          backgroundColor: colors.dullWhite,
+          borderRadius: 10,
+          paddingHorizontal: scale(15),
+        }}
+      />
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          width: '100%',
+          marginTop:verticalScale(15)
+        }}>
+        <CustomButton containerStyle={styles.modal_save_btn} text={t("Cancel")} />
+        <CustomButton containerStyle={styles.modal_save_btn} text={t("save_time")} />
+      </View>
+    
+       
+        {/* <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
                 
               <View style={{alignItems: 'center', justifyContent: 'center'}}>
                 <View
@@ -157,32 +213,7 @@ const TimerMobile = ({label}) => {
                 <Text>15 min</Text>
               </View>
             </View> */}
-          </View>
-
-      {/* <View
-            style={{flexDirection: 'row', alignItems: 'center', marginTop: 30}}>
-            <Text style={{fontSize: 15, fontWeight: '600'}}>Add manually</Text>
-            <View>
-              <TextInput
-                placeholder="+30 min"
-                placeholderTextColor="#CCD4D6"
-                value={manual_time}
-                keyboardType="decimal-pad"
-                // keyboardType="phone-pad"
-                onChangeText={text => set_manual_time(text)}
-                style={{
-                  borderWidth: 1,
-                  borderRadius: 5,
-                  width: 120,
-                  height: 40,
-                  borderColor: '#CCD4D6',
-                  marginLeft: 10,
-                }}
-              />
-            </View>
-          </View> */}
-
-      {/* <View
+              {/* <View
             style={{
               flexDirection: 'row',
               justifyContent: 'space-between',
@@ -242,7 +273,7 @@ const TimerMobile = ({label}) => {
             opacity={1}
             textStyle={{ color: "red" }}
           /> */}
-    </View>
+    </ScrollView>
   );
 };
 
@@ -250,15 +281,12 @@ export default TimerMobile;
 
 const styles = ScaledSheet.create({
   centeredView: {
-    justifyContent: 'center',
-    alignItems: 'center',
     padding: '20@s',
-    width: "100%",
-   
+    width: '100%',
   },
-  heightView:{
+  heightView: {
     height: '10@vs',
-    width: "2@s",
+    width: '2@s',
     backgroundColor: colors.light_grey,
   },
   // modalView: {
@@ -275,12 +303,11 @@ const styles = ScaledSheet.create({
   //   elevation: 0,
   // },
   modal_save_btn: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: colors.primary,
-    width: '50%',
-    padding: '10@vs',
-    borderRadius: '5@vs',
+    borderRadius: '10@vs',
+    width:'48%',
+    height:'40@vs'
   },
-
 });
+
+
+var x = 5
--- packages/SellerNative/src/screens/orders/molecule/SellerToolScreen.js
+++ packages/SellerNative/src/screens/orders/molecule/SellerToolScreen.js
@@ -1,4 +1,4 @@
-import {View, TouchableOpacity} from 'react-native';
+import {View, TouchableOpacity,Text,TextInput} from 'react-native';
 import React, {useState} from 'react';
 import {scale, ScaledSheet, verticalScale} from 'react-native-size-matters';
 import colors from '@SilalApp/common/assets/colors';
@@ -7,10 +7,12 @@ import {useTranslation} from 'react-i18next';
 import Header from '../../../components/Header';
 import fonts from '@SilalApp/common/assets/fonts';
 import images from '../../../../assets/images';
+import {SwipeListView} from 'react-native-swipe-list-view';
 import CustomText from '@SilalApp/common/components/CustomText';
-import {Icon} from 'react-native-elements';
+import {Icon,CheckBox} from 'react-native-elements';
 import CustomModal from '@SilalApp/common/components/native/CustomModal';
 import TimerMobile from '../../../components/TimerMobile';
+import { CustomButton } from '@SilalApp/common/components/native';
 import {
   widthPercentageToDP as width,
   heightPercentageToDP as height,
@@ -20,6 +22,11 @@ const SellerToolScreen = () => {
   const {t} = useTranslation();
   const [contactModal, setContactModal] = useState(false);
   const [modal, setModal] = useState(false);
+  const [noteModal, setNoteModal] = useState(false);
+  const [addNote, setAddNote] = useState(2);
+  const [check, setcheck] = useState(0);
+
+
 
   const dataArray = [
     {
@@ -32,6 +39,7 @@ const SellerToolScreen = () => {
       id: 2,
       text: t('Notes'),
       iconPath: images.noteIcon,
+      onPress: () => setNoteModal(true),
     },
     {
       id: 3,
@@ -47,6 +55,7 @@ const SellerToolScreen = () => {
       onPress: () => setContactModal(true),
     },
   ];
+  const data3 = [{id: 1}];
 
   return (
     <View style={styles.mainContainer}>
@@ -157,7 +166,7 @@ const SellerToolScreen = () => {
           setModalVisible={setModal}
           modalWrapperStyle={{
             marginHorizontal: scale(15),
-            marginVertical: height(31),
+            marginVertical: height(26),
           }}
           modalContainerStyle={{
             borderRadius: 2,
@@ -166,9 +175,180 @@ const SellerToolScreen = () => {
             width: '100%',
             height: '100%',
           }}>
-            <TimerMobile label={t('deliveryEstimateTime')} />
+          <TouchableOpacity
+            style={{position: 'absolute', top: 10, right: 10}}
+            onPress={() => setModal(false)}>
+            <Icon
+              type="entypo"
+              name="cross"
+              color={colors.black}
+              size={verticalScale(20)}
+            />
+          </TouchableOpacity>
+          <TimerMobile label={t('deliveryEstimateTime')} />
         </CustomModal>
       )}
+     {noteModal &&   <CustomModal
+        isModalVisible={noteModal}
+        setModalVisible={setNoteModal}
+        modalWrapperStyle={{
+          
+          justifyContent: 'flex-start',
+          marginHorizontal: width(0),
+          marginVertical: height(0),
+          padding: 10,
+          flex: 1,
+        }}>
+        <View
+          style={{
+            width: '100%',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}>
+          <CustomText label={t('Notes')} textStyle={styles.notesTitle} />
+          <TouchableOpacity
+            activeOpacity={0.6}
+            onPress={() => setNoteModal(false)}>
+            <Icon
+              name="cross"
+              type="entypo"
+              size={24}
+              style={{alignSelf: 'flex-end'}}
+            />
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            height: '93%',
+            justifyContent: 'space-between',
+            width: '100%',
+          }}>
+          {addNote == 0 ? (
+            <View style={{width: '100%'}}>
+              <CustomText
+                label="21 November 2021"
+                textStyle={styles.addNoteText}
+              />
+              <View style={{flexDirection: 'row', alignItems: 'center',height:25}}>
+                <CheckBox
+                  checked={check}
+                  onPress={() => setcheck(0)}
+                  checkedColor="#5AB3A8"
+                  uncheckedColor="red"
+                />
+                <TextInput
+                  style={{
+                    backgroundColor: 'transparent',
+                    borderLeftColor: colors.black,
+                    borderLeftWidth: 1,
+                    height: "100%",
+                  }}
+                />
+              </View>
+            </View>
+          ) : addNote == 1 ? (
+            <View>
+              <CustomText label="Ehllo" />
+            </View>
+          ) : addNote == 2 ? (
+            <>
+              <SwipeListView
+                data={data3}
+                renderItem={(data, rowMap) => (
+                  <View>
+                    <View style={styles.noteDetail}>
+                      <CustomText
+                        label="20 November 2021"
+                        textStyle={styles.date}
+                      />
+                      <CustomText
+                        label="Sed ut perspiciatis unde omnis iste natus error sit volupta..."
+                        textStyle={styles.noteDetailText}
+                      />
+                    </View>
+
+                    <View style={styles.borderView} />
+                  </View>
+                )}
+                renderHiddenItem={(data, rowMap) => (
+                  <View style={styles.rowBack}>
+                    
+                      <TouchableOpacity
+                        style={
+                          {
+                            backgroundColor: '#FB5C5C',
+                           position: 'absolute',
+                          right: 1,
+                          marginTop:2,
+                          width: 85,
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          paddingVertical:verticalScale(18)
+                        }
+                         
+                        }>
+                        <Text style={styles.BackButtonsText}>Delete</Text>
+                      </TouchableOpacity>
+                    </View>
+                )}
+                rightOpenValue={-90}
+              />
+
+              {/* <View style={styles.noteDetail}>
+         
+
+              <View style={{marginHorizontal: 10, marginVertical: 5}}>
+                <CustomText label="17 November 2021" textStyle={styles.date} />
+                <CustomText
+                  label="Sed ut perspiciatis unde omnis iste natus error sit volupta..."
+                  textStyle={styles.noteDetailText}
+                />
+              </View>
+
+              <View style={styles.borderView} />
+              <View style={{marginHorizontal: 10, marginVertical: 5}}>
+                <CustomText label="16 November 2021" textStyle={styles.date} />
+                <CustomText
+                  label="Sed ut perspiciatis unde omnis iste natus error sit volupta..."
+                  textStyle={styles.noteDetailText}
+                />
+              </View>
+
+              <View style={styles.borderView} />
+            </View> */}
+            </>
+          ) : (
+            <View />
+          )}
+          {addNote == 0 ? (
+            <CustomButton
+              text={'Save note'}
+              containerStyle={styles.saveNoteButton}
+              textStyle={styles.textNote}
+              onPress={() => setAddNote(2)}
+            />
+          ) : addNote == 2 ? (
+            <TouchableOpacity activeOpacity={0.6} onPress={() => setAddNote(0)}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'flex-end',
+                }}>
+                <Icon
+                  name="plus"
+                  type="antdesign"
+                  color={colors.white}
+                  size={28}
+                  style={styles.plus}
+                />
+              </View>
+            </TouchableOpacity>
+          ) : (
+            <View />
+          )}
+        </View>
+      </CustomModal>}
     </View>
   );
 };
@@ -212,5 +392,55 @@ const styles = ScaledSheet.create({
     fontSize: 13,
     fontFamily: fonts.LatoMedium,
     color: colors.sidebar,
+  },
+  notesTitle: {
+    fontFamily: fonts.bold,
+    fontSize: '17@ms',
+    color: colors.black,
+  },
+  noteDetail: {
+    width: '100%',
+    backgroundColor: colors.background,
+    alignSelf: 'center',
+    paddingVertical: "10@vs",
+    
+  },
+  noteDetailText: {
+    fontSize: '12@ms',
+    fontFamily: fonts.LatoRegular,
+    color: colors.textPrime,
+  },
+  borderView: {
+    borderBottomColor: colors.borderColor2,
+    borderBottomWidth: '1@s',
+    width: '100%',
+  },
+  rowBack: {
+    height: 55,
+    backgroundColor: '#fff',
+  },
+  BackButtons: {
+    width: 85,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  BackButtonsText: {
+    color: '#fff',
+    fontSize: 15,
+    fontFamily: 'Lato-Medium',
+  },
+  saveNoteButton: {
+    width: '100%',
+    backgroundColor: colors.primaryBlur,
+  },
+  textNote: {
+    fontSize: '15@ms',
+    fontFamily: fonts.PoppinsSemiBold,
+    color: colors.primary,
+  },
+  plus: {
+    backgroundColor: colors.primary,
+    padding: '10@s',
+    borderRadius: '99@s',
   },
 });
