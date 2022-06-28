b9ff282c7cad21269500c4f8d615156fec8f74b7
Mshoaib41 <shoaibbabar9612@gmail.com>
2022-06-28 10:32:00 +0500

    invite

--- packages/SellerNative/src/screens/Setting/molecule/InviteWorkforce/index.js
+++ packages/SellerNative/src/screens/Setting/molecule/InviteWorkforce/index.js
@@ -175,6 +175,108 @@ const InviteWorkforce = ({navigation}) => {
           </View>
         </CustomModal>
       )}
+       {revokeModal && (
+        <CustomModal
+          isModalVisible={revokeModal}
+          setModalVisible={setRevokeModal}
+          modalWrapperStyle={{
+            marginHorizontal: width(3),
+            marginVertical: height(20),
+          }}>
+          <View style={styles.modalMainCont}>
+            <CustomText
+              label={t('grantingAccess')}
+              marginBottom={verticalScale(8)}
+              fontSize={16}
+              fontFamily={fonts.LatoBold}
+              color={colors.black}
+            />
+            <CustomText
+              textStyle={{textAlign: 'center', lineHeight: 24}}
+              label={t('grantingAccessDetails')}
+              marginBottom={verticalScale(15)}
+              fontSize={15}
+              fontFamily={fonts.LatoMedium}
+              color={colors.black}
+            />
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <CustomText
+                fontSize={15}
+                fontFamily={fonts.LatoMedium}
+                color={colors.black}
+                label={t('adminsCan')}
+              />
+              <CustomText
+                label="att1,att1,att1,att1"
+                fontSize={15}
+                fontFamily={fonts.LatoMedium}
+                color={colors.black}
+              />
+            </View>
+            <View style={{height: verticalScale(5)}} />
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <CustomText
+                fontSize={15}
+                fontFamily={fonts.LatoMedium}
+                color={colors.black}
+                label={t('adminsCannot')}
+              />
+              <CustomText
+                label="att1,att1,att1,att1"
+                fontSize={15}
+                fontFamily={fonts.LatoMedium}
+                color={colors.black}
+              />
+            </View>
+            <View style={{height: verticalScale(40)}} />
+
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <CustomText
+                fontSize={15}
+                fontFamily={fonts.LatoMedium}
+                color={colors.black}
+                label={t('adminsCan')}
+              />
+              <CustomText
+                label="att1,att1,att1,att1"
+                fontSize={15}
+                fontFamily={fonts.LatoMedium}
+                color={colors.black}
+              />
+            </View>
+            <View style={{height: verticalScale(5)}} />
+
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <CustomText
+                fontSize={15}
+                fontFamily={fonts.LatoMedium}
+                color={colors.black}
+                label={t('adminsCannot')}
+              />
+              <CustomText
+                label="att1,att1,att1,att1"
+                fontSize={15}
+                fontFamily={fonts.LatoMedium}
+                color={colors.black}
+              />
+            </View>
+            <View style={styles.noteContainer}>
+              <Text style={[styles.noteText]}>
+                {t('noteFromSilal')}
+                <Text style={{color: colors.mehndi50}}>
+                  {t('noteFromSilalDetail')}
+                </Text>
+              </Text>
+            </View>
+            <CustomButton
+              onPress={() => setGrantModal(false)}
+              textStyle={{fontSize: verticalScale(15)}}
+              text={t('Ok')}
+              containerStyle={styles.okBtn}
+            />
+          </View>
+        </CustomModal>
+      )}
     </ScrollView>
   );
 };
